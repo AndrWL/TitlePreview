30b2a13ef21e1a77ab1bb84d64c064eabb7599c0
AndrWL <andrewlebedev13@gmail.com>
2025-08-19 12:47:25 +0300

    Initial Commit

---
+++ TitlePreview/TitlePreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitlePreviewApp.swift
+//  TitlePreview
+//
+//  Created by Developer on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TitlePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
